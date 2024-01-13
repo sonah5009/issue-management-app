01da203092383a0d1f61066b3bee67ad9fe67b36
sonah5009 <sonah5009@gmail.com>
2024-01-13 14:02:01 +0900

    2. 네비게이션 바 생성

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,17 +1,32 @@
 import Link from "next/link";
 import React from "react";
+import { FaTasks } from "react-icons/fa";
 
 const NavBar = () => {
+  const links = [
+    {
+      label: "Dashboard",
+      href: "/",
+    },
+    {
+      label: "Issues",
+      href: "/issues",
+    },
+  ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 items-center h-14">
-      <Link href="/">Logo</Link>
-      <ul className="flex">
-        <li>
-          <Link href="/">Dashboard</Link>
-        </li>
-        <li>
-          <Link href="/issues">Issues</Link>
-        </li>
+      <Link href="/">
+        <FaTasks />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            key={link.href}
+            href={link.href}>
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
