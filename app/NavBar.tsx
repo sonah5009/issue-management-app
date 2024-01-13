a57f8e850ef9cf12bad3e6f09da62fbe549af0b0
sonah5009 <sonah5009@gmail.com>
2024-01-13 14:13:41 +0900

    3. 활성화 링크 구현

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,11 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { FaTasks } from "react-icons/fa";
 
 const NavBar = () => {
+  const currentPath = usePathname(); // browser api 에 의존하므로 client component 로 전환해야 함
   const links = [
     {
       label: "Dashboard",
@@ -21,7 +24,10 @@ const NavBar = () => {
       <ul className="flex space-x-6">
         {links.map((link) => (
           <Link
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            // className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={`${
+              link.href === currentPath ? "text-zinc-800" : "text-zinc-500"
+            } hover:text-zinc-800 transition-colors`}
             key={link.href}
             href={link.href}>
             {link.label}
