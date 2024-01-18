4ff14ff7dc12268d28c9d1efb8a9173811ac3f03
sonah5009 <sonah5009@gmail.com>
2024-01-18 15:37:02 +0900

    11. 이슈 생성 폼 제출 기능 구현

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,18 +1,42 @@
 "use client";
 import { Button, TextArea, TextField } from "@radix-ui/themes";
 import React from "react";
+import { useForm, Controller } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import axios from "axios"; // 데이터 요청에 사용하는 Axios 패키지
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  // input 태그에 입력된 데이터 변경 사항 추정 및 제출에 사용됨
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const router = useRouter();
+  console.log(register("title"));
+
   return (
-    <div className="max-w-xl space-y-3">
+    // 이슈 생성은 HTTP POST method임
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data); // promise 를 return 하므로 Async await 추가
+        router.push("/issues");
+      })}>
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <SimpleMDE placeholder="Description" />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
       <Button>Create New Issue</Button>
-    </div>
+    </form>
   );
 };
 
